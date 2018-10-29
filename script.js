a10daa8474a5f8590a67d93578f391456f34e5e7
lilcaesar <lilcaesar4@gmail.com>
2018-10-29 12:50:53 +0100

    Aggiunti commenti nel file script

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 d3.dsv(';', 'dataset2.csv').then(function (data) {
     // Variables
     var body = d3.select('body');
@@ -7,12 +5,15 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
     var h = 600 - margin.top - margin.bottom;
     var w = 600 - margin.left - margin.right;
 
+    //Scale per gli assi
     var xScale = d3.scaleLinear()
         .domain([0, 1])
         .range([0, w]);
     var yScale = d3.scaleLinear()
         .domain([0.5, 1, 2, 3, 4, 5, 5.5])
-        .range([h, 9*h/10, 7.5*h/10, h/2, 2.5*h/10, h/10, 0]);
+        .range([h, 9 * h / 10, 7.5 * h / 10, h / 2, 2.5 * h / 10, h / 10, 0]);
+
+    //Scala per le label dell'asse delle x
     var xScaleLabels = d3.scaleOrdinal()
         .domain([" ", "Bassa", "Media", "Alta", ""])
         .range([w, xScale(0.165), xScale(0.495), xScale(0.825), 0]);
@@ -23,6 +24,7 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
         .attr('width', w + margin.left + margin.right)
         .append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
 // X-axis
     var xAxis = d3.axisBottom(xScaleLabels);
 // Y-axis
@@ -34,10 +36,9 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
     var lastDate = getLastDate(data);
 
     var finalData = parseCSV(data, firstDate, lastDate);
-    //console.log(finalData);
 
 
-//Functions for setting the circle in foreground or background
+//Funzioni per mettere i cerchi in primo piano o secondo piano
     d3.selection.prototype.moveToFront = function () {
         return this.each(function () {
             this.parentNode.appendChild(this);
@@ -52,19 +53,26 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
         });
     };
 
-//Color scale
+//Scala dei colori basata sul range di valori di punteggio*affidabilita
     var colors = d3.scaleLinear()
         .domain([0, 2, 5])
         .range(["red", "yellow", "green"]);
 
-//Area of the circle based on interpolation
+//Dati per l'interpolazione successiva
     var valMax = d3.max(finalData, function (d) {
         return d.population
     });
     var radiusMin = 500;
     var radiusMax = 10000;
 
-
+    // Funzione che interpola il range di popolazioni dei gruppi con i valori massimi e minimi prescelti per i cerchi
+    function radiusScale(input) {
+        if (input >= 1) {
+            return ((((input - 1) * (radiusMax - radiusMin)) / (valMax - 1)) + radiusMin);
+        } else {
+            return 0;
+        }
+    }
 
 // X-axis
     svg.append('g')
@@ -93,27 +101,38 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
         .style('text-anchor', 'end')
         .text('Punteggio');
 
+    //Funzione per parsare le date da quel formato stringa nel formato interpretabile da d3 (e browser)
     var parseDate = d3.timeParse("%d/%m/%Y %H:%M");
+    //Trasformazione da data a stringa per gli l'asse dello slider
     var formatTimeReadableAxis = d3.timeFormat("%m/%Y");
+    //Trasformazione da data a stringa per l'output in pagina
     var formatTimeReadable = d3.timeFormat("%d/%m/%Y");
+    //Trasformazione da data a stringa per utilizzabile da ParseCSV
     var formatTimeParser = d3.timeFormat("%d/%m/%Y %H:%M");
 
+    //Scale da data a pixel
     var sliderScale = d3.scaleTime()
         .domain([parseDate(firstDate), parseDate(lastDate)])
         .range([0, w]);
+    //Scale da pixel a data
     var sliderScaleINV = d3.scaleTime()
         .domain([0, w])
         .range([parseDate(firstDate), parseDate(lastDate)]);
 
+    //Asse con le label dello slider
     var sliderAxis = d3.axisBottom(sliderScale)
         .tickFormat(formatTimeReadableAxis);
 
+    //Altezza dell'asse dello slider
     var hSlider = h + 30;
+
+    //Append dell'asse nell'SVG
     svg.append('g')
         .attr('class', 'axis')
         .attr('transform', 'translate(0,' + hSlider + ')')
         .call(sliderAxis);
 
+    //Creo lo slider con range firstDate-lastDate
     var slider = createD3RangeSlider(sliderScale(parseDate(firstDate)), sliderScale(parseDate(lastDate)), "#slider-container");
 
     // inizializza la startdate e enddate sugli estremi dello slider
@@ -136,13 +155,13 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
     slider.range(sliderScale(parseDate(firstDate)), sliderScale(parseDate(lastDate)));
 
     // inizializza la startdate e enddate sugli estremi dello slider
-    function createSlidersStartEndDate(){
+    function createSlidersStartEndDate() {
         // startdate
         var node = document.querySelector(".handle.WW"),
             ele = document.createElement("div");
         ele.setAttribute("id", "startdate");
 
-        ele.innerHTML = firstDate.substring(0, firstDate.length-6);
+        ele.innerHTML = firstDate.substring(0, firstDate.length - 6);
         node.appendChild(ele);
 
         // enddate
@@ -150,7 +169,7 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
             ele1 = document.createElement("div");
         ele1.setAttribute("id", "enddate");
 
-        ele1.innerHTML = lastDate.substring(0, lastDate.length-6);
+        ele1.innerHTML = lastDate.substring(0, lastDate.length - 6);
         node1.appendChild(ele1);
     }
 
@@ -231,15 +250,6 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
             });
     }
 
-    // funzione......
-    function radiusScale(input) {
-        if (input >= 1) {
-            return ((((input - 1) * (radiusMax - radiusMin)) / (valMax - 1)) + radiusMin);
-        } else {
-            return 0;
-        }
-    }
-
 
     // aggiorna il grafico e lo slider tenendo conto degli allenamenti dell'ultimo mese
     var month_button = document.querySelector('#last-month');
@@ -247,11 +257,11 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
         var olderMonthDate = lastDate;
 
         // inverto l'ordine del mese e giorno
-        var day = olderMonthDate.substring(0,2);
-        var month = olderMonthDate.substring(3,5);
-        var year = olderMonthDate.substring(6,10);
-        var time = firstDate.substring(11,16);
-        olderMonthDate = month +'/'+day+'/'+year;
+        var day = olderMonthDate.substring(0, 2);
+        var month = olderMonthDate.substring(3, 5);
+        var year = olderMonthDate.substring(6, 10);
+        var time = firstDate.substring(11, 16);
+        olderMonthDate = month + '/' + day + '/' + year;
 
         // credo una data in formato m%/d%/
         olderMonthDate = new Date(olderMonthDate);
@@ -264,8 +274,8 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
 
         var newFirstDate = olderMonthDate;
 
-        slider.range(sliderScale(parseDate(newFirstDate+" "+time)), sliderScale(parseDate(lastDate)));
-        var newLastDate = lastDate.substring(0,10);
+        slider.range(sliderScale(parseDate(newFirstDate + " " + time)), sliderScale(parseDate(lastDate)));
+        var newLastDate = lastDate.substring(0, 10);
 
         // modifica date per il nuovo parsing degli allenamenti
 
@@ -283,11 +293,11 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
         var olderWeekDate = lastDate;
 
         // inverto l'ordine del mese e giorno
-        var day = olderWeekDate.substring(0,2);
-        var month = olderWeekDate.substring(3,5);
-        var year = olderWeekDate.substring(6,10);
-        var time = firstDate.substring(11,16);
-        olderWeekDate = month +'/'+day+'/'+year;
+        var day = olderWeekDate.substring(0, 2);
+        var month = olderWeekDate.substring(3, 5);
+        var year = olderWeekDate.substring(6, 10);
+        var time = firstDate.substring(11, 16);
+        olderWeekDate = month + '/' + day + '/' + year;
 
         // credo una data in formato m%/d%/
         olderWeekDate = new Date(olderWeekDate);
@@ -300,8 +310,8 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
 
         var newFirstDate = olderWeekDate;
 
-        slider.range(sliderScale(parseDate(newFirstDate+" "+time)), sliderScale(parseDate(lastDate)));
-        var newLastDate = lastDate.substring(0,10);
+        slider.range(sliderScale(parseDate(newFirstDate + " " + time)), sliderScale(parseDate(lastDate)));
+        var newLastDate = lastDate.substring(0, 10);
 
         // modifica date per il nuovo parsing degli allenamenti
 
@@ -321,8 +331,8 @@ d3.dsv(';', 'dataset2.csv').then(function (data) {
 
     var reset_button = document.querySelector('#reset');
     reset_button.onclick = function () {
-        firstDate=getFirstDate(data);
-        lastDate=getLastDate(data);
+        firstDate = getFirstDate(data);
+        lastDate = getLastDate(data);
         d3.select("#startdate").text(firstDate);
         d3.select("#enddate").text(lastDate);
         slider.range(sliderScale(parseDate(firstDate)), sliderScale(parseDate(lastDate)));
